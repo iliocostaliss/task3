ad0b0d5960eab315c41c6c05ead9344698959b58
Aleksandra <oliviadupurpur@gmail.com>
2025-02-27 16:49:18 +0500

    widget.py

--- src/widget.py
+++ src/widget.py
@@ -1,7 +1,13 @@
 from typing import Union
-from src.masks import get_mask_card_number, get_mask_account
+
+from src.masks import get_mask_account, get_mask_card_number
+
 
 def mask_account_card(account_info: Union[str]) -> Union[str]:
+    """
+    Функция, которая умеет обрабатывать информацию как о картах, так и о счетах.
+    Принимает на вход строку, содержащую тип (карту или счет) и подбирает подходящую под этот тип маскировку.
+    """
     if account_info.startswith("Счет"):
         account_number = account_info[:5].replace(" ", "")
         masked_number = get_mask_account(account_number)
@@ -14,13 +20,12 @@ def mask_account_card(account_info: Union[str]) -> Union[str]:
         return " ".join(card_number_parts[-1] + masked_number)
 
 
-
 def get_date(date_str: Union[str]) -> Union[str]:
+    """
+    Функция, которая принимает на вход строку с датой в формате "2024-03-11T02:26:18.671407"
+    и возвращает строку с датой в формате "ДД.ММ.ГГГГ" ("11.03.2024").
+    """
     date_part = date_str.split("T")[0]
     day, month, year = date_part.split("-")
     formatted_date = f"{day},{month},{year}"
     return formatted_date
-
-
-
-
