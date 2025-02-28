92d16f38b374015ac31635c648c50bc49e6c5599
Aleksandra <oliviadupurpur@gmail.com>
2025-02-28 19:07:19 +0500

    add task1

--- src/widget.py
+++ src/widget.py
@@ -9,7 +9,7 @@ def mask_account_card(account_info: Union[str]) -> Union[str]:
     Принимает на вход строку, содержащую тип (карту или счет) и подбирает подходящую под этот тип маскировку.
     """
     if account_info.startswith("Счет"):
-        account_number = account_info[:5].replace(" ", "")
+        account_number = account_info[5:].replace(" ", "")
         masked_number = get_mask_account(account_number)
         return f"Счет {masked_number}"
 
@@ -17,7 +17,7 @@ def mask_account_card(account_info: Union[str]) -> Union[str]:
         card_number_parts = account_info.split()
         card_number = card_number_parts[-1]
         masked_number = get_mask_card_number(card_number)
-        return " ".join(card_number_parts[-1] + masked_number)
+        return " ".join(card_number_parts[:-1] + [masked_number])
 
 
 def get_date(date_str: Union[str]) -> Union[str]:
@@ -26,6 +26,6 @@ def get_date(date_str: Union[str]) -> Union[str]:
     и возвращает строку с датой в формате "ДД.ММ.ГГГГ" ("11.03.2024").
     """
     date_part = date_str.split("T")[0]
-    day, month, year = date_part.split("-")
-    formatted_date = f"{day},{month},{year}"
+    year, month, day = date_part.split("-")
+    formatted_date = f"{day}.{month}.{year}"
     return formatted_date
