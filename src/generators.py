67ee9c94ba18187442c34a8d2bf3b3ebd318df5e
Aleksandra <oliviadupurpur@gmail.com>
2025-03-20 15:37:55 +0500

    add generators.py

---
+++ src/generators.py
@@ -0,0 +1,36 @@
+from turtledemo.penrose import start
+from typing import Dict, List, Iterator, Generator
+
+
+def filter_by_currency(transactions: List[Dict], currency: str) -> Iterator[Dict]:
+    """
+    Функция возвращает итератор, который поочередно выдает транзакции, где валюта операции соответствует заданной
+    (например, USD).
+    """
+    for transaction in transactions:
+        if transaction.get("operationAmount", {}).get("currency", {}).get("code") == currency:
+            yield transaction
+
+
+def transaction_descriptions(transactions: List[Dict[str, str]]) -> Generator[str, None, None]:
+    """
+    Генератор, который принимает список словарей с транзакциями и возвращает описание каждой операции по очереди.
+    """
+    for transaction in transactions:
+        yield transaction["description"]
+
+
+def card_number_generator(start: int, end: int) -> Generator[str, None, None]:
+    """
+    Генератор, который выдает номера банковских карт в формате XXXX XXXX XXXX XXXX, где X — цифра номера карты.
+    Может сгенерировать номера карт в заданном диапазоне от 0000 0000 0000 0001 до 9999 9999 9999 9999.
+    Генератор должен принимать начальное и конечное значения для генерации диапазона номеров.
+    """
+    for num in range(start, end + 1):
+        card_number = str(num)
+        card_number = "0" * (16 - (len(card_number))) + card_number
+        formated_card_number = " ".join([card_number[i:i+4] for i in range(0, 16, 4)])
+        yield formated_card_number
+
+
+
