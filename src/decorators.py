ce04c6290c2a32c7782211ed1b29549c5abc69d9
Aleksandra <oliviadupurpur@gmail.com>
2025-03-31 14:31:10 +0500

    add decorator + mylog.txt

---
+++ src/decorators.py
@@ -0,0 +1,38 @@
+from functools import wraps
+from  datetime import datetime
+
+
+def log(filename=None):
+    def decorator(func):
+        def wrapper(*args, **kwargs):
+            """
+            Автоматически логирует начало и конец выполнения функции, а также ее результаты или возникшие ошибки.
+            """
+            func_name = func.__name__
+            date_time = datetime.now().isoformat(sep=" ", timespec="seconds")
+            input_params = f"args: {args}, kwargs: {kwargs}"
+            start_msg = f"{date_time} - {func_name} - Started - {input_params}"
+            _write_log(start_msg, filename)
+
+            try:
+                result = func(*args, **kwargs)
+                finish_msg = f"{date_time} - {func_name} - Finished: {result}"
+                _write_log(finish_msg, filename)
+                return result
+            except Exception as e:
+                error_msg = f"{date_time} - {func_name} - Error: {type(e).__name__} - {str(e)} - Input: {input_params}"
+                _write_log(error_msg, filename)
+                raise
+
+        return wrapper
+    return decorator
+
+
+def _write_log(message, filename=None):
+    """Функция для записи лога в файл или консоль."""
+    if filename:
+        with open(filename, 'a') as f:
+            f.write(message + '\n')
+    else:
+        print(message)
+
